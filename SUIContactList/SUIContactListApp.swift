be1294d677f9b560e0f06de1c895e90acfe14775
Ruslanios22 <ilgamovich@gmail.com>
2022-12-24 18:33:40 +0300

    Initial Commit

---
+++ SUIContactList/SUIContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIContactListApp.swift
+//  SUIContactList
+//
+//  Created by Руслан Шигапов on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
